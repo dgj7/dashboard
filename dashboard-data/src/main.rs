44d4d1dbb2916e69f33383c0db7134292b4bb85a
dgj7 <garciad48879@gmail.com>
2026-06-03 17:22:23 -0400

    data: add database initializer

--- dashboard-data/src/main.rs
+++ dashboard-data/src/main.rs
@@ -1,3 +1,11 @@
+use include_dir::{include_dir, Dir};
+use rusqlite::Connection;
+use rusqlite_migration::Migrations;
+
+static DDL: Dir = include_dir!("$CARGO_MANIFEST_DIR/ddl");
+
 fn main() {
-    println!("Hello, world!");
+    let mut connection = Connection::open("apps.db").unwrap();
+    let migrations = Migrations::from_directory(&DDL).unwrap();
+    migrations.to_latest(&mut connection).unwrap();
 }
